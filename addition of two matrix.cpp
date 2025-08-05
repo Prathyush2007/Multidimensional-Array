0be28b3eba10a0eb771f92d83368b04cbdc68e59
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 15:42:34 +0530

    Update addition of two matrix.cpp

--- addition of two matrix.cpp
+++ addition of two matrix.cpp
@@ -3,35 +3,39 @@ using namespace std;
 
 int main() {
     int rows, cols;
-    cout << "Enter the number of rows: ";
+
+    cout << "Enter number of rows: ";
     cin >> rows;
-    cout << "Enter the number of columns: ";
+    cout << "Enter number of columns: ";
     cin >> cols;
-    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
-    cout << "Enter elements of first matrix:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+
+    int matrix1[100][100], matrix2[100][100], sum[100][100];
+
+    cout << "\nEnter elements of first matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Element [" << i << "][" << j << "]: ";
             cin >> matrix1[i][j];
         }
     }
-    cout << "Enter elements of second matrix:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    cout << "\nEnter elements of second matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Element [" << i << "][" << j << "]: ";
             cin >> matrix2[i][j];
         }
     }
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
             sum[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
-    cout << "Sum of the two matrices:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    cout << "\nSum of the two matrices:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
             cout << sum[i][j] << " ";
         }
         cout << endl;
     }
 
     return 0;
-}
